9e74022cdcfe041497208370179c108989ccfbc5
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2024-04-17 15:35:36 +0500

    Update SignalBanner.tsx

--- app/routes/SignalBanner.tsx
+++ app/routes/SignalBanner.tsx
@@ -21,37 +21,48 @@ const signDataCollection: signDataType[] = [
 ];
 
 // variant is default left   however user can pass right as well  
-const Sign = ({ signData }: { signData: signDataType}) => {
+const Sign = ({ signData , variant }: { signData: signDataType , variant: string} ) => {
 
     //  if variant is right then the logo will be on right side
-
+    
 	return (
 		<div
 			className={cn(
 				"h-1/2 flex flex-col justify-end p-2 py-4 gap-5 ",
 				signData.tailcolor
 			)}
-		>
-			<div className="flex justify-between">
+        >
+            
+			
+            <div className={cn("flex justify-between", variant === "right" ? "flex-row-reverse" : "flex-row" )}>
 				<div id="logo" className="size-24"></div>
-				<div className="flex flex-col">
-					<h1 className="font-bold text-right  text-4xl ">{signData.urdutext}</h1>
-					<h1 className="text-3xl font-bold text-right ">{signData.engtext}</h1>
+				<div className={cn("flex flex-col", variant === "right" ? "text-left" : "text-right" )}>
+					<h1 className="font-bold text-4xl   ">{signData.urdutext}</h1>
+					<h1 className="text-3xl font-bold ">{signData.engtext}</h1>
 				</div>
 			</div>
 		</div>
 	);
 };
 
-const SignalSidebar = ( {variant}:{variant:string}) => {
+const SignalSidebar = ({ variant }: { variant: string }) => {
+    
+    // by default variant is left 
+    // if variant is right then apply flex-row-reverse on div element with id signboard
+
+
 	return (
-		<div className=" flex w-[700px]    border-8 border-solid border-white  justify-center text-white">
+        <div id="signboard" className={cn("flex w-[700px]    border-8 border-solid border-white  justify-center text-white",
+        variant === "right" ? "flex-row-reverse" : "flex-row"
+        )}
+        
+        >
 			<div className="w-4/12  bg-amber-500 items-center justify-center flex">
                 <div className="size-24 bg-slate-500">{ variant}</div>
 			</div>
-			<div className="w-8/12 flex flex-col ">
+			<div className="w-8/12 flex flex-col   ">
 				{signDataCollection.map((item, index) => (
-					<Sign key={index} signData={item}  />
+					<Sign key={index} signData={item} variant={variant}  />
 				))}
 			</div>
 		</div>
@@ -60,8 +71,8 @@ const SignalSidebar = ( {variant}:{variant:string}) => {
 
 const SignalBanner = () => {
 	return (
-		<div className="w-full items-center flex justify-center bg-slate-500">
-			<SignalSidebar variant="right" />
+		<div className="w-full items-center flex justify-center bg-slate-500 p-5">
+			<SignalSidebar variant="left" />
 		</div>
 	);
 };
