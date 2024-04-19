ad4062c473ac94e01f64636ab7f30e1952e1b689
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2024-04-19 16:26:57 +0500

    greenbuisness top responsive

---
+++ app/routes/greenbuisness.tsx
@@ -0,0 +1,31 @@
+const greenbuisness = () => {
+	return (
+    <div className="container bg-orange-200">
+      
+			<div className="flex  flex-col    bg-teal-950 text-white  justify-center  mx-auto ">
+			
+        <div className="flex flex-col  w-full  p-12 ">
+					<h1 className=" text-6xl   font-bold  text-white  pb-6">
+						Selling online is vital to business growth.
+					</h1>
+					<h2 className="text-2xl prose">2025 Grow Your e-Commerce Conference</h2>
+          <div className="mt-28">
+          <span className="block">September 16-18, 2025</span>
+					<span className="block">Paragon One Plaza and Sky Garden2</span>
+          </div>
+					<button className="btn uppercase    mt-8 bg-yellow-500 rounded-full   w-fit  text-black py-3  px-8 font-bold ">
+						BOOK NOW FOR EARLY BIRD PRICES
+					</button>
+				</div>
+				
+        <div className="w-full  bg-sky-200 ">
+					<img className="h-full object-cover" src="https://fakeimg.pl/700x450" alt="" />
+				</div>
+      
+      </div>
+      
+		</div>
+	);
+};
+
+export default greenbuisness;
