ddcb58dcd467a9ccbd75554e3fa29576f5d09252
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2024-04-20 14:03:49 +0500

    Update greenbuisness.tsx

--- app/routes/greenbuisness.tsx
+++ app/routes/greenbuisness.tsx
@@ -1,19 +1,21 @@
 const greenbuisness = () => {
 	return (
-    <div className="container bg-orange-200">
+    <div className="container bg-[#0c3b2e]">
       
-			<div className="flex  flex-col    bg-teal-950 text-white  justify-center  mx-auto ">
+			<div className="flex  flex-col  xl:flex-row   bg-[#0c3b2e] text-white  justify-center  mx-auto ">
 			
-        <div className="flex flex-col  w-full  p-12 ">
-					<h1 className=" text-6xl   font-bold  text-white  pb-6">
-						Selling online is vital to business growth.
+        <div className="flex flex-col  w-full  pt-5 pl-10 pr-5 ">
+					<h1 className=" text-4xl   font-extrabold  text-white  pb-6 sm:text-5xl  md:text-6xl  ">
+            <span className="block min-w-full">Selling online</span>
+            <span className="block min-w-full">is vital to</span>
+            <span className="block min-w-full">business growth.</span>
 					</h1>
-					<h2 className="text-2xl prose">2025 Grow Your e-Commerce Conference</h2>
+					<h2 className="text-xl ">2025 Grow Your e-Commerce Conference</h2>
           <div className="mt-28">
           <span className="block">September 16-18, 2025</span>
 					<span className="block">Paragon One Plaza and Sky Garden2</span>
           </div>
-					<button className="btn uppercase    mt-8 bg-yellow-500 rounded-full   w-fit  text-black py-3  px-8 font-bold ">
+					<button className="btn uppercase  mb-4  mt-8 bg-yellow-500 rounded-full   w-fit  text-black py-3  px-8 font-bold ">
 						BOOK NOW FOR EARLY BIRD PRICES
 					</button>
 				</div>
