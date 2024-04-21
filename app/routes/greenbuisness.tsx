ea8117e9e2fb34ae0e0083f652edce5cacd23d71
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2024-04-21 09:10:52 +0500

    Update greenbuisness.tsx

--- app/routes/greenbuisness.tsx
+++ app/routes/greenbuisness.tsx
@@ -1,5 +1,6 @@
 const greenbuisness = () => {
 	return (
+<<<<<<< Updated upstream
     <div className="container bg-[#0c3b2e]">
       
 			<div className="flex  flex-col  xl:flex-row   bg-[#0c3b2e] text-white  justify-center  mx-auto ">
@@ -18,14 +19,30 @@ const greenbuisness = () => {
 					<button className="btn uppercase  mb-4  mt-8 bg-yellow-500 rounded-full   w-fit  text-black py-3  px-8 font-bold ">
 						BOOK NOW FOR EARLY BIRD PRICES
 					</button>
+=======
+		<div className="container bg-pink-200 ">
+			<div className="flex flex-col  bg-teal-950  text-white pl-14">
+				<h1 className=" text-7xl   font-bold  text-white  pb-6">
+					Selling online is vital to business growth.
+				</h1>
+				<h2 className="text-2xl prose">2025 Grow Your e-Commerce Conference</h2>
+				<div className="mt-28">
+					<span className="block">September 16-18, 2025</span>
+					<span className="block">Paragon One Plaza and Sky Garden2</span>
+>>>>>>> Stashed changes
 				</div>
-				
-        <div className="w-full  bg-sky-200 ">
-					<img className="h-full object-cover" src="https://fakeimg.pl/700x450" alt="" />
-				</div>
-      
-      </div>
-      
+				<button className="btn uppercase    mt-8 bg-yellow-500 rounded-full   w-fit  text-black py-3  px-8 font-bold ">
+					BOOK NOW FOR EARLY BIRD PRICES
+				</button>
+			</div>
+
+			<div className="  bg-sky-200 ">
+				<img
+					className="h-full object-cover w-full"
+					src="https://fakeimg.pl/700x450"
+					alt=""
+				/>
+			</div>
 		</div>
 	);
 };
